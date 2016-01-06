5b8b1b2402260e18a0040fb924d2ca838249e9df
Ethan Barhydt <ethanbarhydt@gmail.com>
2016-01-05 20:20:27 -0500

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: 'AKIAJRCTY3ULEUUYIY7A',
-    aws_secret_access_key: '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'
-  }
-  config.fog_directory = "print-from-anywhere"
-end
